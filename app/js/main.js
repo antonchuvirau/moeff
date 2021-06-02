cfe2dd46333f1fa03d1b898d451381475ad3c282
antonchuvirau <anton_chuvirau@icloud.com>
2021-06-03 00:29:57 +0300

    All pages were added

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const heroCarouselElement = document.querySelector(`.hero__carousel-box`);
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    if (heroCarouselElement) {
+        new Swiper(heroCarouselElement, {
+            pagination: {
+                el: heroCarouselElement.nextElementSibling,
+                clickable: true
+            }
+        });
+    }
+});
