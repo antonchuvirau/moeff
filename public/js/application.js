ac0c04b7d36f15e9454c1c19296768ce13fb195c
antonchuvirau <anton_chuvirau@icloud.com>
2021-06-03 20:11:31 +0300

    All pages were added

--- public/js/application.js
+++ public/js/application.js
@@ -14,13 +14,33 @@
 'use strict';
 
 const heroCarouselElement = document.querySelector(`.hero__carousel-box`);
+const productGalleryElement = document.querySelector(`.product-details__gallery-box`);
+const productThumbsElement = document.querySelector(`.product-details__thumbs-box`);
 
 document.addEventListener(`DOMContentLoaded`, () => {
     if (heroCarouselElement) {
         new Swiper(heroCarouselElement, {
+            autoplay: {
+                delay: 3500
+            },
             pagination: {
                 el: heroCarouselElement.nextElementSibling,
                 clickable: true
+            }
+        });
+    }
+    if (productGalleryElement) {
+        new Swiper(productGalleryElement, {
+            pagination: {
+                el: productGalleryElement.nextElementSibling,
+                clickable: true
+            },
+            thumbs: {
+                swiper: {
+                    el: productThumbsElement,
+                    slidesPerView: `auto`,
+                    spaceBetween: 10
+                }
             }
         });
     }
